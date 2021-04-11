dbc8cadc9602db1cd2d0e775c77fe3eeb06ad806
Tiernan8r <tiernan8r@pm.me>
2021-04-11 13:50:49 +0100

    code dump

--- src/me/Tiernanator/Flight/FlightCommand.java
+++ src/me/Tiernanator/Flight/FlightCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.Tiernanator.Colours.Colour;
+import me.Tiernanator.Utilities.Colours.Colour;
 
 public class FlightCommand implements CommandExecutor {
 
